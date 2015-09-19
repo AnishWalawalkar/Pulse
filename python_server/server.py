e3d484320c569d881a8d145a80e4eb4ee3bd00dd
Anish Walawalkar <aswalawa@uwaterloo.ca>
2015-09-19 17:25:51 -0400

    updates to server code

--- python_server/server.py
+++ python_server/server.py
@@ -4,16 +4,18 @@ from flask import request
 import indicoio
 import json
 
+indicoio.config.api_key = 'b1b29987309a10beab53d428a70699d3'
+
 app = Flask(__name__)
 
+
+
 def analysis(data):
-    return data
+    return indicoio.analyze_text(data.get('data_to_analyze'))
 
 
 @app.route('/', methods = ['GET', 'POST'])
 def get_data():
-    if request.method == 'GET':
-        return 'Hello World'
     if request.method == 'POST':
         data_dict = dict(request.form)
         analysed_data = analysis(data_dict)
