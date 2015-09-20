ef3f4f8b9a8c8a6cdd7e7304daeda52f13c60851
Anish Walawalkar <aswalawa@uwaterloo.ca>
2015-09-19 21:16:48 -0400

    updated popup.js to make individual post requests

--- chrome/popup.js
+++ chrome/popup.js
@@ -7,7 +7,7 @@ window.onload = function() {
             if(request.action == 'read') {
                 console.log('here');
                 $.post('http://localhost:5000/keywords', {'data_to_analyze': request.data}, function(resp) {
-		    console.log(resp);
+		          console.log(resp);
                     resp = JSON.parse(resp);
                     //populate popup
 		    //send to contentjs
@@ -15,7 +15,24 @@ window.onload = function() {
 			chrome.tabs.sendMessage(tabs[0].id, {action: "readKeywords", keywords: resp.keywords});
 		    });
                 });
-         }
+
+                $.post('http://localhost:5000/sentiment', {'data_to_analyze': request.data}, function(resp) {
+                    console.log(resp);
+                    resp = JSON.parse(resp);
+                });
+                $.post('http://localhost:5000/language', {'data_to_analyze': request.data}, function(resp) {
+                    console.log(resp);
+                    resp = JSON.parse(resp);
+                });
+                $.post('http://localhost:5000/political', {'data_to_analyze': request.data}, function(resp) {
+                    console.log(resp);
+                    resp = JSON.parse(resp);
+                });
+                $.post('http://localhost:5000/text_tags', {'data_to_analyze': request.data}, function(resp) {
+                    console.log(resp);
+                    resp = JSON.parse(resp);
+                });
+        }
     });
 };
 
