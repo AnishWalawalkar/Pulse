76d0ed3e556c67d243c1097daf075c7ebcec5887
Kaivalya Gandhi <kaivalya.gandhi@gmail.com>
2015-09-20 02:48:21 -0400

    edit page support

--- chrome/content.js
+++ chrome/content.js
@@ -15,14 +15,31 @@ function highlight(container, set, spanClass) {
 (function() { setTimeout(function() {
 
     chrome.runtime.onMessage.addListener(function (request, sender, sendResponse){
-        if(request.action == "readPage"){
-	    var text = $(".postArticle p").text();
-	    chrome.runtime.sendMessage({data: text, action:"read"});
-	    console.log("ready");
-        }
+    if(request.action == "readPage"){
+    	console.log("readPage");
+    var text = $(".postArticle p").text();
+    chrome.runtime.sendMessage({data: text, action:"read"});
+    console.log("ready");
+    }
 	if (request.action == "readKeywords") {
+		console.log("readKeywords");
 	   var keywordSet = new Set(request.keywords);
 	   var paragraphs = $(".postArticle-content p");
+	   for (j = 0; j < paragraphs.length; j++) {
+	      highlight(paragraphs[j], keywordSet, "highlight");
+	   }
+	}
+	if(request.action == "writePage"){
+	console.log("writePage");
+    var text = $(".section--body p").text();
+    console.log(text);
+    chrome.runtime.sendMessage({data: text, action:"write"});
+    console.log("ready");
+    }
+	if (request.action == "writeKeywords") {
+		console.log("writeKeywords");
+	   var keywordSet = new Set(request.keywords);
+	   var paragraphs = $(".section-inner p");
 	   for (j = 0; j < paragraphs.length; j++) {
 	      highlight(paragraphs[j], keywordSet, "highlight");
 	   }
--- chrome/popup.js
+++ chrome/popup.js
@@ -3,9 +3,12 @@
 window.onload = function() {
     console.log("loaded");
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    	if (tabs[0].url.indexOf("edit"))
+    	console.log(tabs);
+    	if (tabs[0].url.indexOf("edit") != -1)
     	{
-    		chrome.tabs.sendMessage(tabs[0].id, {action:"editPage"});	
+    		console.log("edit");
+    		chrome.tabs.sendMessage(tabs[0].id, {action:"writePage"});	
+    		console.log("after edit");
     	}
     	else
     	{
@@ -47,7 +50,45 @@ window.onload = function() {
                     $('#text_tag2').text(resp.text_tags[1][0]);
                     $('#text_tag3').text(resp.text_tags[2][0]);
                 });
-        }
+	        }
+
+	        if(request.action == 'write') {
+                console.log('there');
+                $.post('http://localhost:5000/keywords', {'data_to_analyze': request.data}, function(resp) {
+		          console.log(resp);
+                    resp = JSON.parse(resp);
+                    //populate popup
+		    //send to contentjs
+		    chrome.tabs.query({active:true, currentWindow:true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {action: "writeKeywords", keywords: resp.keywords});
+		    });
+                });
+
+                $.post('http://localhost:5000/sentiment', {'data_to_analyze': request.data}, function(resp) {
+                    resp = JSON.parse(resp);
+                    console.log(resp.sentiment);
+                    img_src = "Img/emojis/" + resp.sentiment + ".png"
+                    $('#sentiment').prepend('<img src = ' + img_src +' width="60" height="60">');
+                });
+                $.post('http://localhost:5000/language', {'data_to_analyze': request.data}, function(resp) {
+                    resp = JSON.parse(resp);
+                    $('#language').text(resp.language[0]);
+                });
+                $.post('http://localhost:5000/political', {'data_to_analyze': request.data}, function(resp) {
+                    console.log(resp);
+                    resp = JSON.parse(resp);
+                });
+                $.post('http://localhost:5000/text_tags', {'data_to_analyze': request.data}, function(resp) {
+                    resp = JSON.parse(resp);
+                    $('#text_tag1').text(resp.text_tags[0][0]);
+                    $('#text_tag2').text(resp.text_tags[1][0]);
+                    $('#text_tag3').text(resp.text_tags[2][0]);
+                });
+	        }
+
+
+
+
     });
 };
 
