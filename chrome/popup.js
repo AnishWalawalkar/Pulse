1a542a99302798b183c0bf9de6977cc4ef1b5a18
ayodejiige <ayodeji18@outlook.com>
2015-09-20 02:33:26 -0400

    updates

--- chrome/popup.js
+++ chrome/popup.js
@@ -5,8 +5,7 @@ window.onload = function() {
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         chrome.tabs.sendMessage(tabs[0].id, {action:"readPage"})
     });
-	var ctx = document.getElementById("chart-area").getContext("2d");
-	window.myPie = new Chart(ctx).Doughnut(pieData);
+	
     chrome.runtime.onMessage.addListener( function(request, sender, sendResponse) {
             if(request.action == 'read') {
                 console.log('here');
@@ -33,6 +32,34 @@ window.onload = function() {
                 $.post('http://localhost:5000/political', {'data_to_analyze': request.data}, function(resp) {
                     console.log(resp);
                     resp = JSON.parse(resp);
+                    var pieData = [
+				{
+					value: resp.political[0].Libertarian,
+					color: "#FDB45C",
+					highlight: "#FFC870",
+					label: "Libertarian"
+				},
+				{
+					value: resp.political[0].Liberal,
+					color: "#949FB1",
+					highlight: "#A8B3C5",
+					label: "Liberal"
+				},
+				{
+					value: resp.political[0].Conservative,
+                    color:"#F7464A",
+					highlight: "#FF5A5E",
+					label: "Conservative"
+				},
+				{
+					value: resp.political[0].Green,
+					color: "#46BFBD",
+					highlight: "#5AD3D1",
+					label: "Green"
+				}
+			];
+                    var ctx = document.getElementById("chart-area").getContext("2d");
+	                window.myPie = new Chart(ctx).Doughnut(pieData);
                 });
                 $.post('http://localhost:5000/text_tags', {'data_to_analyze': request.data}, function(resp) {
                     resp = JSON.parse(resp);
@@ -44,30 +71,5 @@ window.onload = function() {
     });
 };
 
-var pieData = [
-				{
-					value: 300,
-					color: "#FDB45C",
-					highlight: "#FFC870",
-					label: "Libertarian"
-				},
-				{
-					value: 50,
-					color: "#46BFBD",
-					highlight: "#5AD3D1",
-					label: "Liberal"
-				},
-				{
-					value: 100,
-                    color:"#F7464A",
-					highlight: "#FF5A5E",
-					label: "Conservative"
-				},
-				{
-					value: 40,
-					color: "#949FB1",
-					highlight: "#A8B3C5",
-					label: "Green"
-				}
-			];
+
 
