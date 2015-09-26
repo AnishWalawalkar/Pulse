70ea20c1df775606c51934f4e8d5d3813b4fe469
Yifan Song <y75song@uwaterloo.ca>
2015-09-25 20:35:51 -0400

    slight clean up

--- python_server/server.py
+++ python_server/server.py
@@ -13,29 +13,6 @@ def sort(data):
     sorted_data = sorted(data.items(), key=op.itemgetter(1))
     return sorted_data[::-1]
 
-# def analysis(data):
-#     data = data['data_to_analyze']
-#     sentiment = indicoio.sentiment(data)
-#     language = sort(indicoio.language(data)[0])
-#     political = indicoio.political(data)
-#     text_tags = sort(indicoio.text_tags(data)[0])[0:3]
-#     keywords = indicoio.keywords(data, top_n = 15, threshold = 0.2, relative=True)[0].keys()
-#     return {
-#         'sentiment': sentiment,
-#         'language': language,
-#         'political': political,
-#         'text_tags': text_tags,
-#         'keywords': keywords
-#     }
-
-# @app.route('/', methods = ['GET', 'POST'])
-# def get_data():
-#     if request.method == 'POST':
-#         data_dict = dict(request.form)
-#         analysed_data = analysis(data_dict)
-#         print analysed_data
-#         return json.dumps(analysed_data)
-
 @app.route('/sentiment', methods=['GET', 'POST'])
 def get_sentiment():
     if request.method == 'POST':
